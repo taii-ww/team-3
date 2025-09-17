91cfc8b94c9178eb8e2f7f1462bfc63ffd08e32b
thiendoanh <ngunhubobnm10000@gmail.com>
2025-09-17 16:19:08 +0700

    Update bai4.java

--- BAITHTUAN4/bai4.java
+++ BAITHTUAN4/bai4.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class bai4 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner nhap = new Scanner(System.in);
         System.out.print("Nhap n: ");
-        int n = sc.nextInt();
-        int sum = 0;
+        int n = nhap.nextInt();
+        int tong = 0;
 
         for (int i = 1; i <= n; i++) {
-            sum += i;
+            tong += i;
         }
 
-        System.out.println("Tong tu 1 den " + n + " la: " + sum);
+        System.out.println("Tong tu 1 den " + n + " la: " + tong);
         sc.close();
     }
 }
+
